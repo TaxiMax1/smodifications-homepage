24db0bf2e8ae4ac4b577a362822faebd65b957e1
TaxiMax1 <taximaxi1808@gmail.com>
2025-02-19 02:05:17 -0300

    Fix GitHub Pages deployment

--- dist/src/App.jsx
+++ dist/src/App.jsx
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/smodifications-homepage">
+    <Router>
       <Routes>
         <Route path="/" element={<MainLayout />} />
         <Route path="/signup" element={<SignupOnly />} />
