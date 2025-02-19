2ed2f7d1057d190478de8867e783e74ff0d656a4
TaxiMax1 <taximaxi1808@gmail.com>
2025-02-19 02:12:13 -0300

    Fix GitHub Pages deployment

--- dist/src/App.jsx
+++ dist/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./sections/Navbar.jsx";
 import Footer from "./sections/Footer.jsx";
 import MainSection from "./sections/Main.jsx"; 
